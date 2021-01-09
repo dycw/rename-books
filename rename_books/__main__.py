c0c9b560c5d0d0c7c2050269e379ceaa9d86058b
Derek Wan <d.wan@icloud.com>
2021-01-09 12:34:43 +0800

    Improve again

--- rename_books/__main__.py
+++ rename_books/__main__.py
@@ -1,4 +1,3 @@
-import re
 from contextlib import suppress
 from logging import basicConfig
 from logging import getLogger
@@ -42,44 +41,52 @@ def process_name(
 ) -> None:
     LOGGER.info(f"Processing {path.name}...")
     while True:
-        input_year = input("Input year ('q' to quit): ")
-        if input_year == "q":
+        if (input_year := input("Input year ('q' to quit): ")) == "q":
             raise Quit()
+        elif match := search(r"^(\d+)$", input_year):
+            year = int(match.group(1))
+            break
         else:
-            pattern = re.compile(r"^(\d+)$")
-            if match := pattern.search(input_year):
-                year = int(match.group(1))
-                break
-            else:
-                LOGGER.info(
-                    f"Year must match {pattern}; {input_year!r} does not",
-                )
-    title = input("Input title ('q' to quit): ")
-    if title == "q":
-        raise Quit()
-    new_name = f"{year} — {title}"
-    input_subtitle = input("Input subtitle (optional; 'q' to quit): ")
-    if input_subtitle == "":
-        pass
-    elif input_subtitle == "q":
-        raise Quit()
-    else:
-        new_name = "–".join([new_name, input_subtitle])
+            LOGGER.info(f"{input_year!r} is an invalid year")
+    while True:
+        if (input_title := input("Input title ('q' to quit): ")) == "q":
+            raise Quit()
+        elif match := search(r"^(\w+)$", input_title):
+            title = match.group(1)
+            break
+        else:
+            LOGGER.info(f"{input_title!r} is an invalid title")
+    new_name = " — ".join([str(year), title])
+    while True:
+        if (input_subtitle := input("Input subtitle ('q' to quit): ")) == "q":
+            raise Quit()
+        elif input_subtitle == "":
+            break
+        elif match := search(r"^(\w+)$", input_subtitle):
+            subtitle = match.group(1)
+            new_name = "–".join([new_name, subtitle])
+            break
+        else:
+            LOGGER.info(f"{input_subtitle!r} is an invalid subtitle")
     authors: List[str] = []
     while True:
         next_n = len(authors) + 1
-        input_author = input(f"Input author #{next_n} ('q' to quit):")
-        if input_author == "":
+        if (
+            input_author := input(f"Input author #{next_n} ('q' to quit):")
+        ) == "":
+            raise Quit()
+        elif input_author == "":
             if authors:
                 joined_authors = ", ".join(authors)
                 new_name = " ".join([new_name, joined_authors])
                 break
             else:
                 raise Quit()
-        elif input_author == "q":
-            raise Quit()
+        elif match := search(r"^(\w+)$", input_author):
+            authors.append(match.group(1))
+            break
         else:
-            authors.append(input_author)
+            LOGGER.info(f"{input_author!r} is an invalid author")
     while True:
         input_confirm = input(f"Confirm new name:\n{new_name}? ('y'/'n')")
         if input_confirm == "y":
