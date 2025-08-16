5cb0d24f08ac8db46f099327a57d80c756db388d
Derek Wan <d.wan@icloud.com>
2025-08-16 23:23:05 +0900

    <derekwan/RH-Mac/20250816T232305 (Sat)>

--- src/rename_books/constants.py
+++ src/rename_books/constants.py
@@ -2,13 +2,7 @@ from __future__ import annotations
 
 from pathlib import Path
 
-
-def _get_path_dropbox() -> Path:
-    """Get the path to Dropbox."""
-    return Path.home().joinpath("Dropbox")
-
-
-DROPBOX = _get_path_dropbox()
+DROPBOX = Path.home().joinpath("Dropbox")
 BOOKS_AND_PAPERS = DROPBOX.joinpath("1 – Derek", "Books and papers")
 BOOKS = BOOKS_AND_PAPERS.joinpath("Books")
 TEMPORARY_PATH = DROPBOX.joinpath("Temporary")
