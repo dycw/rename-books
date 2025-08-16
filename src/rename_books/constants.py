3164e7c29f54bb0a2f78fce15f764a1d5ce3fce5
Derek Wan <d.wan@icloud.com>
2025-08-16 23:22:53 +0900

    <derekwan/RH-Mac/20250816T232253 (Sat)>

--- src/rename_books/constants.py
+++ src/rename_books/constants.py
@@ -1,19 +1,11 @@
 from __future__ import annotations
 
 from pathlib import Path
-from socket import gethostname
 
 
 def _get_path_dropbox() -> Path:
     """Get the path to Dropbox."""
-    if (hostname := gethostname()) == "DW-Acer":
-        return Path("/mnt/c/Documents and Settings/Derek/Dropbox")
-    if hostname == "DW-NUC":
-        return Path("/data/Dropbox")
-    if hostname in {"DW-Mac", "DW-Laptop", "DW-PC", "DW-PC-Ubuntu"}:
-        return Path.home().joinpath("Dropbox")
-    msg = f"Invalid {hostname=}"
-    raise RuntimeError(msg)
+    return Path.home().joinpath("Dropbox")
 
 
 DROPBOX = _get_path_dropbox()
